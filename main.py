cbc29c35d06897fdc818afefe45b41482f14482d
Ganesh Chandrasekaran <gchandra@gmail.com>
2024-03-26 16:46:20 -0400

    Update

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-from faker import Faker
-fake = Faker()
-
-def add(a, b):
-    return a + b
-
-
-def subtract(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    return a / b
+from calc import add, subtract, multiply, divide
 
 print(add(1, 2))
 print(subtract(1, 2))
 print(multiply(1, 2))
 print(divide(1, 2))
-
-print(fake.name(), fake.address())
